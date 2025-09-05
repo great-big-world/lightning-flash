2667c7f395344b0953559feeb57036d4d4a1c84b
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-09-04 20:15:01 -0400

    Initial commit

---
+++ src/main/java/dev/creoii/lightningflash/client/LightningFlashClient.java
@@ -0,0 +1,22 @@
+package dev.creoii.lightningflash.client;
+
+import dev.creoii.lightningflash.util.ExtendedLivingEntity;
+import dev.creoii.lightningflash.util.StruckByLightningS2C;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+
+import java.util.UUID;
+
+public class LightningFlashClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ClientPlayNetworking.registerGlobalReceiver(StruckByLightningS2C.PACKET_ID, (struckByLightningS2C, context) -> {
+            UUID uuid = struckByLightningS2C.uuid();
+            context.client().execute(() -> {
+                if (context.client().world.getEntity(uuid) instanceof ExtendedLivingEntity extendedLivingEntity) {
+                    extendedLivingEntity.gbw$setStruckByLightningTime(10);
+                }
+            });
+        });
+    }
+}
---
+++ src/main/java/dev/creoii/lightningflash/util/ExtendedLivingEntity.java
@@ -0,0 +1,7 @@
+package dev.creoii.lightningflash.util;
+
+public interface ExtendedLivingEntity {
+    int gbw$getStruckByLightningTime();
+
+    void gbw$setStruckByLightningTime(int struckByLightningTime);
+}
