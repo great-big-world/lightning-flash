6ab2c6adfb2493c4426da9584c30d108002313fa
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-11-16 04:40:45 -0500

    Update to 1.21.10

--- src/main/java/dev/creoii/lightningflash/mixin/client/LivingEntityRendererMixin.java
+++ src/main/java/dev/creoii/lightningflash/mixin/client/LivingEntityRendererMixin.java
@@ -5,11 +5,15 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.creoii.lightningflash.util.ExtendedLivingEntity;
 import dev.creoii.lightningflash.util.ExtendedLivingEntityRenderState;
+import net.minecraft.client.model.Model;
 import net.minecraft.client.render.OverlayTexture;
-import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.command.ModelCommandRenderer;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.state.LivingEntityRenderState;
+import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Colors;
@@ -21,7 +25,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntityRenderer.class)
 public class LivingEntityRendererMixin {
-    @WrapOperation(method = "render(Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;getAnimationCounter(Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;)F"))
+    @WrapOperation(method = "render(Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/command/OrderedRenderCommandQueue;Lnet/minecraft/client/render/state/CameraRenderState;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;getAnimationCounter(Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;)F"))
     private <T extends LivingEntity, S extends LivingEntityRenderState, M extends EntityModel<? super S>> float gbw$wrapOverlayForLightningOverlay(LivingEntityRenderer<T, S, M> instance, S state, Operation<Float> original, @Local(argsOnly = true) S livingEntityRenderState) {
         if (livingEntityRenderState instanceof ExtendedLivingEntityRenderState extended && extended.gbw$isStruckByLightning()) {
             return 1f;
@@ -29,11 +33,11 @@ public class LivingEntityRendererMixin {
         return original.call(instance, state);
     }
 
-    @WrapOperation(method = "render(Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;III)V"))
-    private <S extends LivingEntityRenderState> void gbw$wrapRenderForLightningOverlay(EntityModel<? super S> instance, MatrixStack matrixStack, VertexConsumer vertexConsumer, int i, int j, int l, Operation<Void> original, @Local(argsOnly = true) S livingEntityRenderState) {
+    @WrapOperation(method = "render(Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/command/OrderedRenderCommandQueue;Lnet/minecraft/client/render/state/CameraRenderState;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/command/OrderedRenderCommandQueue;submitModel(Lnet/minecraft/client/model/Model;Ljava/lang/Object;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/RenderLayer;IIILnet/minecraft/client/texture/Sprite;ILnet/minecraft/client/render/command/ModelCommandRenderer$CrumblingOverlayCommand;)V"))
+    private <S extends LivingEntityRenderState> void gbw$wrapRenderForLightningOverlay(OrderedRenderCommandQueue instance, Model<S> model, Object o, MatrixStack matrixStack, RenderLayer renderLayer, int l, int i, int k, Sprite sprite, int outlineColor, ModelCommandRenderer.CrumblingOverlayCommand crumblingOverlayCommand, Operation<Void> original, @Local(argsOnly = true) S livingEntityRenderState) {
         if (livingEntityRenderState instanceof ExtendedLivingEntityRenderState extended && extended.gbw$isStruckByLightning()) {
-            original.call(instance, matrixStack, vertexConsumer, i, j, Colors.WHITE);
-        } else original.call(instance, matrixStack, vertexConsumer, i, j, l);
+            original.call(instance, model, o, matrixStack, renderLayer, l, Colors.WHITE, k, sprite, outlineColor, crumblingOverlayCommand);
+        } else original.call(instance, model, o, matrixStack, renderLayer, l, i, k, sprite, outlineColor, crumblingOverlayCommand);
     }
 
     @Inject(method = "updateRenderState(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getEquippedStack(Lnet/minecraft/entity/EquipmentSlot;)Lnet/minecraft/item/ItemStack;"))
